0cb6a23e1ed7dedc9b88e80cc241102051099fa5
Erik Andreas Barreto de Vera <erikandreasdev@proton.me>
2025-02-12 16:03:38 +0000

    Initialize Spring Boot

---
+++ src/main/java/com/erikandreas/exchangedataservice/ExchangeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erikandreas.exchangedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erikandreas/exchangedataservice/ExchangeDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikandreas.exchangedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
